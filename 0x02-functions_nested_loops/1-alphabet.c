0c5198eb3be54e8ba3c941a8a0ab835956410b83
JARNEEX <ogunfuyejeh@gmail.com>
2022-12-14 08:25:14 -0800

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - prints main
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+  _putchar('h');
+  _putchar('o');
+  _putchar('l');
+  _putchar('b');
+  _putchar('e');
+  _putchar('r');
+  _putchar('t');
+  _putchar('e');
+  _putchar('n');
+  return (0)'
+}
