695798ddc3cc7de89dbfdae97dfb0bc6e32fc920
JARNEEX <ogunfuyejeh@gmail.com>
2022-12-15 13:57:57 -0800

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; 1 <= 14; J++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
@@ -19,4 +19,3 @@ void more_numbers(void)
 		_putchar('\n');
 	}
 }
-x
